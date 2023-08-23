ad86093e7b9cfea6685427fc6c0e110b894f6a5b
Cole Dennis <coleedennis@gmail.com>
2023-08-23 08:32:22 -0500

    Initial Commit

---
+++ PathfinderV3/PathfinderV3App.swift
@@ -0,0 +1,21 @@
+//
+//  PathfinderV3App.swift
+//  PathfinderV3
+//
+//  Created by Cole Dennis on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct PathfinderV3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
